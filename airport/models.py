285772ee20d2ea4b5198f6ebb2a2fa3883f1bc9b
HamsterKate <131155602+HamsterKate@users.noreply.github.com>
2026-08-02 10:53:31 +0000

    Create Airport model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} ({self.closest_big_city})"
+
+
+
