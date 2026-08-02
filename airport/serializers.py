e9f83cc76bef99a7d9a7d1ca18f04b5eb2645e58
HamsterKate <131155602+HamsterKate@users.noreply.github.com>
2026-08-02 20:19:01 +0000

    create Flight serializers

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,4 +1,4 @@
-from django.db import transactions
+from django.db import transaction
 from rest_framework import serializers
 
 from airport.models import (
@@ -57,4 +57,84 @@ class RouteSerializer(serializers.ModelSerializer):
         fields = (
             "id", "source", "destination", "distance",
         )
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    duration = serializers.DurationField(read_only=True)
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+            "duration",
+            "crew",
+        )
+
+
+class FlightListSerializer(serializers.ModelSerializer):
+    route = serializers.StringRelatedField()
+    airplane = serializers.CharField(
+        source="airplane.name",
+        read_only=True
+    )
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+            "duration",
+        )
+
+
+class FlightDetailSerializer(serializers.ModelSerializer):
+    route = RouteSerializer(read_only=True)
+    airplane = AirplaneSerializer(read_only=True)
+    crew = CrewSerializer(
+        many=True,
+        read_only=True
+    )
+    duration = serializers.DurationField(read_only=True)
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "crew",
+            "departure_time",
+            "arrival_time",
+            "duration",
+        )
+
+
+class FlightCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flight
+        fields = (
+            "route",
+            "airplane",
+            "crew",
+            "departure_time",
+            "arrival_time",
+        )
+
+
+
+
+
+
+
+
+
+
 
